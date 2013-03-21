3346565bb5f7b8684d61f3ba22752a9cfa6ae53a
Forrest Alvarez <forrest.alvarez@gmail.com>
2013-03-21 03:32:06 +0000

    updated directory structure in prep for catVsDog work, and resolving issues with zipfSong.

---
+++ zipfsong/test/test_zipfSong.py
@@ -0,0 +1,18 @@
+#/usr/bin/python2.6
+
+import unittest
+import sys
+
+sys.path.insert(0, '../')
+
+class zipfSongTestCase(unittest.TestCase):
+
+    def test_simpleList(self):
+
+    def test_complexList(self):
+
+
+    def test_equalPopularity(self):
+
+
+    def test_checkAllSongs(self):
